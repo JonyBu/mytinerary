2e27bacc2144120f15ad707e2ee79f2f07a561de
JonyBu <jaf320@gmail.com>
2020-01-07 20:58:24 -0300

    casa 07012020

--- cliente/src/components/itineraries/favorite.jsx
+++ cliente/src/components/itineraries/favorite.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import Footer from '../footer';
+import { connect } from 'react-redux';
+import outLogin from '../../redux/actions/logoutAction';
+import getUser from '../../redux/actions/getUserAction';
 
-const Favorite = () => {
-    return (
-        <div >
-            <h1>Itinerarios Favoritos</h1>
-            <Footer />
-        </div>
+class Favorite extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentUser: [],
+            isConected: []
+        }
+    }
 
-    );
+    async componentWillMount() {
+        await this.props.getUser(this.state)
+        this.setState({ currentUser: this.props.loginReducer.currentUser, isConected: this.props.loginReducer.isConected })
+    }
+    render(){
+        console.log(this.state);
+        
+        return (
+            <div >
+                <h1>Itinerarios Favoritos</h1>
+                <Footer />
+            </div>
+        );
+    }
 };
 
-export default Favorite;
+const mapStateToProps = (state) => {
+    return {
+        loginReducer: state.loginReducer
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        outLogin: (data) => dispatch(outLogin(data)),
+        getUser: (data) => dispatch(getUser(data)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Favorite);
