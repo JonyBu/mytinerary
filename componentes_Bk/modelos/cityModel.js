6e98edb4cdd2ebb3efc6e9e9a6447c5ae77ad742
JonyBu <jaf320@gmail.com>
2021-01-18 22:46:24 -0300

    Updates

--- componentes_Bk/modelos/cityModel.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require("mongoose");
-
-let citySchema = new mongoose.Schema({
-  name: String,
-  country: String,
-  itineraries: Boolean,
-});
-
-module.exports = mongoose.model("cities", citySchema);
