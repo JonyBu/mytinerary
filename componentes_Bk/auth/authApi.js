0cfe3877fdc203bd1c3fbef94bfeb904fc3bdfd3
JonyBu <jaf320@gmail.com>
2021-01-27 20:37:41 -0300

    heroku ayth

--- componentes_Bk/auth/authApi.js
+++ componentes_Bk/auth/authApi.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
+let express = require("express");
+let router = express.Router();
+let passport = require("passport");
 require("./passPGoogle");
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
